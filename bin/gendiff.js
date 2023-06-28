3d675921977d2a6c2b8154403b9ff5d269791510
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-06-28 17:57:56 +0300

    Add -f, format and option manuals

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,6 @@ import { program } from "commander";
   program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0', '-V, --version', 'output the version number')
-
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1> <filepath2>')
 program.parse();
