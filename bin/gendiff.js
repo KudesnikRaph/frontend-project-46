b33f821db3faa3a0c970ad285d0056e751a7c340
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-06-28 14:31:41 +0300

    Upd gendiff, add description and version

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+import { program } from "commander";
+
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0', '-V, --version', 'output the version number')
+
+program.parse();
