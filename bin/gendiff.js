6c18a25d9df3fcc33492dffa536526def467abd7
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-07-02 23:45:08 +0300

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,13 +3,11 @@ import { program } from "commander";
 import gendiff from "../src/index.js";
 
   program
+  
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0', '-V, --version', 'output the version number')
     .option('-f, --format <type>', 'output format')
     .argument('<filepath1> <filepath2>')
-    .action((file1, file2, options) => {
-      const { format } = options;
-      console.log(gendiff(file1, file2, format));
-    });
+    .action(gendiff);
   
   program.parse();
