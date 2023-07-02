38606f7cb77d581e9e6b3567cf70f4f5a8cdc95b
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-07-02 23:45:43 +0300

    new logic for index

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
-import * as fs from 'node:fs';
-import _ from 'lodash';
+#!/usr/bin/env node
+import fs from "fs";
+import _ from "lodash";
 
 const stringify = (value, replacer = ' ', spacesCount = 1) => {
-  const iter = (el, counter) => {
-    const arr = Object.entries(el).map(([key, val]) => {
-      if (typeof val === 'object' && val) return (`${replacer.repeat(counter)}${key}: ${iter(val, counter + spacesCount)}`);
+  const iteration = (el, counter) => {
+    const array = Object.entries(el).map(([key, val]) => {
+      if (typeof val === 'object' && val) return (`${replacer.repeat(counter)}${key}: ${iteration(val, counter + spacesCount)}`);
       return (`${replacer.repeat(counter)}${key}: ${val}`);
     });
-    const result = arr.join('\n');
-    return counter === spacesCount ? `{\n${result}\n}` : `{\n${result}\n${replacer.repeat(counter - spacesCount)}}`;
+    const output = array.join('\n');
+    return counter === spacesCount ? `{\n${output}\n}` : `{\n${output}\n${replacer.repeat(counter - spacesCount)}}`;
   };
-  return typeof value !== 'object' ? `${value}` : iter(value, spacesCount);
+  return typeof value !== 'object' ? `${value}` : iteration(value, spacesCount);
 };
 
 const gendiff = (file1, file2) => {
-  const result = {};
-  const data1 = JSON.parse(fs.readFileSync(file1, 'utf8'));
-  const data2 = JSON.parse(fs.readFileSync(file2, 'utf8'));
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const sortedUnionKeys = _.sortBy(_.union(keys1, keys2), (k) => k);
-  sortedUnionKeys.forEach((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-    if (value1 === value2) result[`  ${key}`] = value1;
-    if (keys1.includes(key) && !keys2.includes(key)) result[`- ${key}`] = value1;
-    if (!keys1.includes(key) && keys2.includes(key)) result[`+ ${key}`] = value2;
-    if (keys1.includes(key) && keys2.includes(key) && value1 !== value2) {
-      result[`- ${key}`] = value1;
-      result[`+ ${key}`] = value2;
+  const output = {};
+  const value1 = JSON.parse(fs.readFileSync(file1, 'utf8'));
+  const value2 = JSON.parse(fs.readFileSync(file2, 'utf8'));
+  const row1 = Object.keys(value1);
+  const row2 = Object.keys(value2);
+  const sortedUnionRows = _.sortBy(_.union(row1, row2), (k) => k);
+  sortedUnionRows.forEach((key) => {
+    const value1 = value1[key];
+    const value2 = value2[key];
+    if (value1 === value2) output[`  ${key}`] = value1;
+    if (row1.includes(key) && !row2.includes(key)) output[`- ${key}`] = value1;
+    if (!row1.includes(key) && row2.includes(key)) output[`+ ${key}`] = value2;
+    if (row1.includes(key) && row2.includes(key) && value1 !== value2) {
+      output[`- ${key}`] = value1;
+      output[`+ ${key}`] = value2;
     }
   });
-  console.log(stringify(result));
+  console.log(stringify(output));
 };
 export default gendiff;
