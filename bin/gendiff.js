4e32eba48122d8d92ba9f7fef78747ced6ae6944
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-07-14 00:05:10 +0300

    Separation arguments in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,12 +2,15 @@
 import { program } from "commander";
 import gendiff from "../src/index.js";
 
+
+
   program
   
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0', '-V, --version', 'output the version number')
     .option('-f, --format <type>', 'output format')
-    .argument('<filepath1> <filepath2>')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .action(gendiff);
   
   program.parse();
