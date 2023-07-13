a5ee4aa8231f40cda8c4475bbef4422e6a9fda8c
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-07-14 00:07:57 +0300

    Refactoring code in index.js, add comments

--- src/index.js
+++ src/index.js
@@ -14,22 +14,25 @@ const stringify = (value, replacer = ' ', spacesCount = 1) => {
   return typeof value !== 'object' ? `${value}` : iteration(value, spacesCount);
 };
 
+// Чтение содержимого файлов
 const gendiff = (file1, file2) => {
   const output = {};
   const value1 = JSON.parse(fs.readFileSync(file1, 'utf8'));
   const value2 = JSON.parse(fs.readFileSync(file2, 'utf8'));
-  const row1 = Object.keys(value1);
+  // Получение списка ключей из двух объектов
+  const row1 = Object.keys(value1); 
   const row2 = Object.keys(value2);
   const sortedUnionRows = _.sortBy(_.union(row1, row2), (k) => k);
+  console.log(sortedUnionRows)
+  // Обработка каждого ключа
   sortedUnionRows.forEach((key) => {
-    const value1 = value1[key];
-    const value2 = value2[key];
-    if (value1 === value2) output[`  ${key}`] = value1;
-    if (row1.includes(key) && !row2.includes(key)) output[`- ${key}`] = value1;
-    if (!row1.includes(key) && row2.includes(key)) output[`+ ${key}`] = value2;
-    if (row1.includes(key) && row2.includes(key) && value1 !== value2) {
-      output[`- ${key}`] = value1;
-      output[`+ ${key}`] = value2;
+    const keyValue1 = value1[key];
+    const keyValue2 = value2[key];
+    if (keyValue1 === keyValue2) output[`  ${key}`] = keyValue1;
+    if (row1.includes(key) && !row2.includes(key)) output[`- ${key}`] = keyValue1;
+    if (!row1.includes(key) && row2.includes(key)) output[`+ ${key}`] = keyValue2;
+    if (row1.includes(key) && row2.includes(key) && keyValue1 !== keyValue2) {
+      ["-", "+"].forEach(symbol => output[`${symbol} ${key}`] = keyValue1)
     }
   });
   console.log(stringify(output));
