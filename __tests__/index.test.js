07ad5259c8ea6236673ac983b36b207f7adc103d
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-07-15 18:47:52 +0300

    Add tests, it's works

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,19 +1,22 @@
-import gendiff from 'gendiff.js';
-
-describe('gendiff', () => {
-  const filePath1 = './test-data/file1.json';
-  const filePath2 = './test-data/file2.json';
-
-  test('Should return expected result for stylish format', () => {
-    const expected = '...'; // Здесь ожидаемый результат для формата 'stylish'
-    const result = gendiff(filePath1, filePath2, 'stylish');
-    expect(result).toEqual(expected);
-  });
-
-  test('Should return expected result for default format', () => {
-    const expected = '...'; // Здесь ожидаемый результат для формата по умолчанию (если предусмотрен)
-    const result = gendiff(filePath1, filePath2);
-    expect(result).toEqual(expected);
-  });
-
+/* eslint-disable max-len */
+// Импортируем модуль для тестирования
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { test, expect } from '@jest/globals';
+// Импортируем функцию gendiff из вашего кода
+import gendiff from '../src/index.js';
+// Определяем пути к файлам с данными
+const file1 = './bin/file1.json';
+const file2 = './bin/file2.json';
+// Определяем ожидаемый результат сравнения
+const expected = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+// Написываем тест, который вызывает функцию gendiff с файлами и сравнивает ее вывод с ожидаемым результатом
+test('gendiff compares flat json files correctly', () => {
+  expect(gendiff(file1, file2)).toEqual(expected);
 });
