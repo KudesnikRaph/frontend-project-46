178ebc3f911b7618819ba9d9b4cae9bcb0220ac3
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-07-15 18:48:06 +0300

    Upd index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import fs from 'fs';
 import _ from 'lodash';
 
-const stringify = (value, replacer = ' ', spacesCount = 1) => {
+const stringify = (value, replacer = ' ', spacesCount = 2) => {
   const iteration = (el, counter) => {
     const array = Object.entries(el).map(([key, val]) => {
       if (typeof val === 'object' && val) return (`${replacer.repeat(counter)}${key}: ${iteration(val, counter + spacesCount)}`);
@@ -24,7 +24,7 @@ const gendiff = (file1, file2) => {
   const row1 = Object.keys(value1);
   const row2 = Object.keys(value2);
   const sortedUnionRows = _.sortBy(_.union(row1, row2), (k) => k);
-  console.log(sortedUnionRows);
+  // console.log(sortedUnionRows);
   // Обработка каждого ключа
   sortedUnionRows.forEach((key) => {
     const keyValue1 = value1[key];
@@ -33,9 +33,10 @@ const gendiff = (file1, file2) => {
     if (row1.includes(key) && !row2.includes(key)) output[`- ${key}`] = keyValue1;
     if (!row1.includes(key) && row2.includes(key)) output[`+ ${key}`] = keyValue2;
     if (row1.includes(key) && row2.includes(key) && keyValue1 !== keyValue2) {
-      ['-', '+'].forEach((symbol) => output[`${symbol} ${key}`] = keyValue1);
+      output[`- ${key}`] = keyValue1; // значение из первого объекта
+      output[`+ ${key}`] = keyValue2; // значение из второго объекта
     }
   });
-  console.log(stringify(output));
+  return (stringify(output));
 };
 export default gendiff;
