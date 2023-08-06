186e8d83f4d9bd8a39b6a4e91b1b260b3369e158
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-08-07 00:36:46 +0300

    Upd test with fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,14 +13,7 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '.', '__fixtures__', filename);
 
 describe('basic', () => {
-  const expected = `{
-  - follow: false
-  host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+  const expected = '{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}';
 
   test('json type', () => {
     const file1json = 'file1.json';
