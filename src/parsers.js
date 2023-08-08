dfbc7f992e1b09cc6a383b3501124fcb9a55134f
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-08-08 21:29:10 +0300

    Minor fix

--- src/parsers.js
+++ src/parsers.js
@@ -1,12 +1,11 @@
-/* eslint-disable no-unused-vars */
 import * as fs from 'fs';
 import * as path from 'path';
 import * as yaml from 'js-yaml';
 
-export default (pathToFile) => {
-  const fileExt = path.extname(pathToFile);
+export default (filePath) => {
+  const fileExt = path.extname(filePath);
   if (fileExt === '.yml' || fileExt === '.yaml') {
-    return yaml.load(fs.readFileSync(pathToFile, 'utf-8'));
+    return yaml.load(fs.readFileSync(filePath, 'utf-8'));
   }
-  return JSON.parse(fs.readFileSync(pathToFile, 'utf-8'));
+  return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 };
