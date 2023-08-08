aadd150748b936bf8700dd77cd985d16d8b0a0b2
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-08-08 21:26:33 +0300

    Upd index.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,10 @@
-#!/usr/bin/env node
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-return-assign */
-// eslint-disable-next-line import/no-unresolved
-import _ from 'lodash';
+import { buildTree } from './treeBuilder.js';
 import parse from './parsers.js';
-// eslint-disable-next-line import/no-unresolved
-import stylishDiff from './stylish.js';
+import format from './formatters/index.js';
 
-const stringify = (value, replacer = ' ', spacesCount = 2) => {
-  const iteration = (el, counter) => {
-    const array = Object.entries(el).map(([key, val]) => {
-      if (typeof val === 'object' && val) return (`${replacer.repeat(counter)}${key}: ${iteration(val, counter + spacesCount)}`);
-      return (`${replacer.repeat(counter)}${key}: ${val}`);
-    });
-    const output = array.join('\n');
-    return counter === spacesCount ? `{\n${output}\n}` : `{\n${output}\n${replacer.repeat(counter - spacesCount)}}`;
-  };
-  return typeof value !== 'object' ? `${value}` : iteration(value, spacesCount);
+const genDiff = (file1, file2, formatName = 'stylish') => {
+  const data1 = parse(file1);
+  const data2 = parse(file2);
+  return format(formatName)(buildTree(data1, data2));
 };
-
-// Чтение содержимого файлов
-const gendiff = (file1, file2) => {
-  const output = {};
-  // читает два файла в формате
-  const value1 = parse(file1);
-  const value2 = parse(file2);
-
-  // Получение списка ключей из двух объектов
-  const row1 = Object.keys(value1);
-  const row2 = Object.keys(value2);
-  const sortedUnionRows = _.sortBy(_.union(row1, row2), (k) => k);
-  // console.log(sortedUnionRows);
-  // Обработка каждого ключа
-  sortedUnionRows.forEach((key) => {
-    const keyValue1 = value1[key];
-    const keyValue2 = value2[key];
-    if (keyValue1 === keyValue2) output[`${key}`] = keyValue1;
-    if (row1.includes(key) && !row2.includes(key)) output[`- ${key}`] = keyValue1;
-    if (!row1.includes(key) && row2.includes(key)) output[`+ ${key}`] = keyValue2;
-    if (row1.includes(key) && row2.includes(key) && keyValue1 !== keyValue2) {
-      output[`- ${key}`] = keyValue1; // значение из первого объекта
-      output[`+ ${key}`] = keyValue2; // значение из второго объекта
-    }
-  });
-  return (stringify(output));
-};
-export default gendiff;
+export default genDiff;
