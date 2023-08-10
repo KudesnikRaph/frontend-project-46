fa1620884dd74bb30a5f3db31c5dddc2c927b4ef
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-08-10 18:35:45 +0300

    Upd plain.js crate new logic

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,40 +1,20 @@
-const stringifyVal = (val) => {
-  const type = typeof val;
-  if (type === 'string') return `'${val}'`;
-  if (val !== null && type === 'object') {
-    return '[complex value]';
-  }
-  return val;
-};
+import _ from 'lodash';
 
-const mapping = {
-  root: ({ children }) => {
-    const result = children.flatMap((node) => {
-      if (node.type !== 'unchanged') {
-        return mapping[node.type](node, '');
-      }
-      return '';
-    });
-    return result.join('\n');
-  },
-  nested: ({ key, children }, path) => {
-    const result = children.flatMap((node) => {
-      if (node.type !== 'unchanged') {
-        return mapping[node.type](node, `${path}${key}.`);
-      }
-      return '';
-    });
-    return result.join('\n');
-  },
-  added: (node, path) => `Property '${path}${node.key}' was added with value: ${stringifyVal(node.value)}`,
-  deleted: (node, path) => `Property '${path}${node.key}' was removed`,
-  changed: (node, path) => `Property '${path}${node.key}' was updated. From ${stringifyVal(node.value1)} to ${stringifyVal(node.value2)}`,
-};
-
-const workerTree1 = (ast) => {
-  const iter = (node, path) => mapping[node.type](node, path);
-  const output = iter(ast, '');
-  return output.split('\n').filter((line) => line !== '').join('\n');
-};
-
-export default workerTree1;
+export default (plainDiff) => {
+  const firstString = (mainPath) => `Property '${mainPath}'`;
+  const added = (addedVal) => {
+    if (addedVal === '[complex value]') return ` was added with value: ${addedVal}`;
+    if (_.isString(addedVal)) return ` was added with value: '${addedVal}'`;
+    return ` was added with value: ${addedVal}`;
+  };
+  const removed = ' was removed';
+  const updated = (oldMeaning, newMeaning) => {
+    const values = [oldMeaning, newMeaning];
+    const newValues = values.reduce((row, val) => {
+      if (_.isObject(val)) return [...row, '[complex value]'];
+      if (_.isString(val)) return [...row, `'${val}'`];
+      return [...row, `${val}`];
+    }, []);
+    return ` was updated. From ${newValues[0]} to ${newValues[1]}`;
+  };
+  const createString = (start, condition) => `${start}${condition}`;
