108a53c29e8c8b9ad4f35cd0b58d4c937f7b42e8
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-08-10 18:39:37 +0300

    Upd plain.js/you can work with this/step 7

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -18,3 +18,40 @@ export default (plainDiff) => {
     return ` was updated. From ${newValues[0]} to ${newValues[1]}`;
   };
   const createString = (start, condition) => `${start}${condition}`;
+
+  const iter = (tree, currentPath) => {
+    const keys = Object.keys(tree);
+    const result = keys.reduce((row, key, index) => {
+      const splitKey = key.split(' ');
+      const mark = splitKey[0];
+      const currentKey = splitKey[splitKey.length - 1];
+      const value = tree[key];
+
+      const splitedKey = keys[index + 1] ? keys[index + 1].split(' ') : [];
+      const actualedKey = splitedKey[splitedKey.length - 1];
+      const currentValue = tree[splitedKey.join(' ')];
+
+      const previousSplitedKey = keys[index - 1] ? keys[index - 1].split(' ') : [];
+      const previousActualedKey = previousSplitedKey[previousSplitedKey.length - 1];
+
+      if (currentKey === previousActualedKey) return row;
+
+      const newCurrentPath = [...currentPath, currentKey];
+      const strPath = newCurrentPath.join('.');
+
+      if (currentKey === actualedKey) {
+        return [...row, createString(firstString(strPath), updated(value, currentValue))];
+      }
+      if (mark !== '+' && mark !== '-' && _.isObject(value)) return [...row, iter(value, newCurrentPath)];
+      if (currentKey !== actualedKey) {
+        if (mark === '-' || mark === '+') {
+          return mark === '-' ? [...row, createString(firstString(strPath), removed)] : [...row, createString(firstString(strPath), _.isObject(value) ? added('[complex value]') : added(value))];
+        }
+      }
+      return row;
+    }, []);
+    return result.join('\n');
+  };
+
+  return iter(plainDiff, []).trimEnd();
+};
