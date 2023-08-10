c5502b1ca7a1c4f53b8d494a9886d321961dcfcf
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-08-10 18:34:19 +0300

    Upd index.test.js add test for plain/ step 7

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -73,14 +73,36 @@ describe('trees', () => {
         fee: 100500
     }
 }`;
+
   test('json type', () => {
     const tree1json = 'tree1.json';
     const tree2json = 'tree2.json';
     expect(gendiff(getFixturePath(tree1json), getFixturePath(tree2json))).toEqual(result);
   });
+
   test('yaml type', () => {
     const tree1yml = 'tree1.yml';
     const tree2yml = 'tree2.yml';
     expect(gendiff(getFixturePath(tree1yml), getFixturePath(tree2yml))).toEqual(result);
+  });
+});
+
+describe('plain formatters', () => {
+  const plainResult = `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`;
+
+  test('plain', () => {
+    const plainFile1 = 'tree1.json';
+    const plainFile2 = 'tree2.json';
+    expect(gendiff(getFixturePath(plainFile1), getFixturePath(plainFile2), 'plain')).toEqual(plainResult);
   });
 });
