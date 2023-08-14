4ef7cee82da2896f67a24f7d5ea15618d2e09f3d
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-08-14 14:25:45 +0300

    Add formatters JSON/ Step 8

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,6 @@
 import format from './stylish.js';
 import workerTree1 from './plain.js';
+import JSON from './json.js';
 
 export default (formatName) => {
   switch (formatName) {
@@ -7,6 +8,8 @@ export default (formatName) => {
       return format;
     case 'plain':
       return workerTree1;
+    case 'json':
+      return JSON;
 
     default:
       throw new Error('Unknown formatter');
